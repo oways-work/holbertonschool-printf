be56ff2dc51885ef92decd9e204b3507bc20a486
oways-work <owayswork@gmail.com>
2025-10-01 11:53:30 +0000

    all files of task zero

---
+++ _printf.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/**
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+  va_list args;
+  int i = 0, count = 0, j;
+  char *str;
+
+  if (format == NULL)
+    return (-1);
+
+  va_start(args, format);
+  while (format && format[i])
+    {
+      if (format[i] == '%')
+	{
+	  i++;
+	  switch (format[i])
+	    {
+	    case 'c':
+	      _putchar(va_arg(args, int));
+	      count++;
+	      break;
+	    case's':
+	      str = va_arg(args, char *);
+	      if (str == NULL)
+		str = "(null)";
+	      for (j = 0; str[j]; j++)
+		{
+		  _putchar(str[j]);
+		  count++;
+		}
+	      break;
+	    case '%':
+	      _putchar('%');
+	      count++;
+	      break;
+	    default:
+	      _putchar('%');
+	      _putchar(format[i];
+		       count += 2;
+		       break;
+		       }
+	    }
+	  else
+	    }
+      _putchar(format[i]);
+      count++;
+    }
+  i++;
+}
+va_end(args);
+return (count);
