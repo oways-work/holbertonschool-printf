35de35bc8a70e42a1a61fa3faf31f6e7cc702343
oways-work <owayswork@gmail.com>
2025-10-01 15:08:45 +0300

    code fix, adding documentation and improving the code structure for _printf.c

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _printf - Produces output according to a format.
  * @format: The format string.
@@ -7,52 +8,53 @@
  */
 int _printf(const char *format, ...)
 {
-  va_list args;
-  int i = 0, count = 0, j;
-  char *str;
+	va_list args;
+	int i = 0, count = 0, j;
+	char *str;
 
-  if (format == NULL)
-    return (-1);
+	if (format == NULL)
+		return (-1);
 
-  va_start(args, format);
-  while (format && format[i])
-    {
-      if (format[i] == '%')
+	va_start(args, format);
+	while (format && format[i])
 	{
-	  i++;
-	  switch (format[i])
-	    {
-	    case 'c':
-	      _putchar(va_arg(args, int));
-	      count++;
-	      break;
-	    case's':
-	      str = va_arg(args, char *);
-	      if (str == NULL)
-		str = "(null)";
-	      for (j = 0; str[j]; j++)
+		if (format[i] == '%')
 		{
-		  _putchar(str[j]);
-		  count++;
+			i++; /* Move past the '%' */
+			switch (format[i])
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(null)";
+					for (j = 0; str[j]; j++)
+					{
+						_putchar(str[j]);
+						count++;
+					}
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default: /* Handle unknown specifiers */
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+					break;
+			}
 		}
-	      break;
-	    case '%':
-	      _putchar('%');
-	      count++;
-	      break;
-	    default:
-	      _putchar('%');
-	      _putchar(format[i];
-		       count += 2;
-		       break;
-		       }
-	    }
-	  else
-	    }
-      _putchar(format[i]);
-      count++;
-    }
-  i++;
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
-va_end(args);
-return (count);
