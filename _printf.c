b1aca6c787683b59917b09e65957769aabc79729
oways-work <owayswork@gmail.com>
2025-10-01 15:17:48 +0300

    betty compatibility for _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+static int handle_specifier(char specifier, va_list args);
+
 /**
  * _printf - Produces output according to a format.
  * @format: The format string.
@@ -11,38 +13,20 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i, count = 0;
-	char *str;
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
-			i++; /* Move past '%' to the specifier */
-			if (format[i] == '\0') /* Check for dangling '%' */
+			i++;
+			if (format[i] == '\0')
 				return (-1);
-			switch (format[i])
-			{
-				case 'c':
-					count += _putchar(va_arg(args, int));
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					if (str == NULL)
-						str = "(null)";
-					while (*str)
-						count += _putchar(*str++);
-					break;
-				case '%':
-					count += _putchar('%');
-					break;
-				default:
-					count += _putchar('%');
-					count += _putchar(format[i]);
-					break;
-			}
+
+			count += handle_specifier(format[i], args);
 		}
 		else
 		{
@@ -50,5 +34,40 @@ int _printf(const char *format, ...)
 		}
 	}
 	va_end(args);
+	return (count);
+}
+
+/**
+ * handle_specifier - Prints an argument based on a format specifier.
+ * @specifier: The character that specifies the format.
+ * @args: The va_list containing the arguments.
+ *
+ * Return: The number of characters printed.
+ */
+static int handle_specifier(char specifier, va_list args)
+{
+	int count = 0;
+	char *str;
+
+	switch (specifier)
+	{
+		case 'c':
+			count += _putchar(va_arg(args, int));
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (str == NULL)
+				str = "(null)";
+			while (*str)
+				count += _putchar(*str++);
+			break;
+		case '%':
+			count += _putchar('%');
+			break;
+		default:
+			count += _putchar('%');
+			count += _putchar(specifier);
+			break;
+	}
 	return (count);
 }
