5548974e364eca1ad5fac57918cb014d0d7eba2d
rateelbahathiq <rateelbahathiq@gmail.com>
2025-10-03 11:57:14 +0000

    keep only _printf and handle_specifier in _printf.c(No more than 5 functions per file)

--- _printf.c
+++ _printf.c
@@ -1,10 +1,7 @@
 #include "main.h"
 
 static int handle_specifier(char specifier, va_list args);
-static int print_int(va_list args);
-static int print_unsigned_rec(unsigned int n);
-static int print_binary(unsigned int n) __attribute__((unused));             
-static int print_base(unsigned int n, int base, int uppercase);
+
 
 /**
  * _printf - Produces output according to a format.
@@ -81,7 +78,7 @@ static int handle_specifier(char specifier, va_list args)
 			break;
 		case 'x':
 			count += print_base(va_arg(args, unsigned int), 16, 0);
-			break;
+			brek;
 		case 'X':
 			count += print_base(va_arg(args, unsigned int), 16, 1);
 			break;
@@ -90,118 +87,5 @@ static int handle_specifier(char specifier, va_list args)
 			count += _putchar(specifier);
 			break;
 	}
-	return (count);
-}
-
-/**
- * print_unsigned_rec - Prints an unsigned integer recursively.
- * @n: The unsigned integer to print.
- *
- * Return: The number of characters printed.
- */
-static int print_unsigned_rec(unsigned int n)
-{
-	int count = 0;
-
-	if (n / 10)
-		count += print_unsigned_rec(n / 10);
-
-	count += _putchar((n % 10) + '0');
-	return (count);
-}
-
-/**
- * print_int - Prints a signed integer.
- * @args: The va_list containing the integer argument.
- *
- * Return: The number of characters printed.
- */
-static int print_int(va_list args)
-{
-	int n = va_arg(args, int);
-	unsigned int u;
-	int count = 0;
-
-	if (n < 0)
-	{
-		count += _putchar('-');
-		u = (unsigned int)(-(n + 1)) + 1;
-	}
-	else
-	{
-		u = (unsigned int)n;
-	}
-
-	count += print_unsigned_rec(u);
-	return (count);
-}
-
-/**
- * print_binary - Converts an unsigned int to binary and prints it.
- * @n: The unsigned integer to convert.
- *
- * Return: The number of characters printed.
- */
-static int print_binary(unsigned int n)
-{
-	int count = 0;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-
-	if (n / 2)
-		count += print_binary(n / 2);
-
-	count += _putchar((n % 2) + '0');
-
-	return (count);
-}
-
-/**
- * print_unsigned - Prints an unsigned decimal integer (%u).
- * @args: The va_list containing the unsigned int.
- *
- * Return: The number of characters printed.
- */
-static int print_unsigned(va_list args)
-{
-	unsigned int u = va_arg(args, unsigned int);
-
-
-	return (print_unsigned_rec(u));
-}
-/**
- * print_base - Prints an unsigned int in a given base (8 or 16 for this task).
- * @n: The number.
- * @base: Base to print (8 or 16).
- * @uppercase: If non-zero, use 'A'-'F'; else 'a'-'f' for hex.
- *
- * Return: The number of characters printed.
- */
-static int print_base(unsigned int n, int base, int uppercase)
-{
-	int count = 0;
-	char digit;
-
-	if (base != 8 && base != 16)
-		return (0);
-
-	if (n == 0)
-		return (_putchar('0'));
-
-	if (n / (unsigned int)base)
-		count += print_base(n / (unsigned int)base, base, uppercase);
-
-	n %= (unsigned int)base;
-
-	if (n < 10)
-		digit = (char)('0' + n);
-	else
-		digit = (char)((uppercase ? 'A' : 'a') + (n - 10));
-
-	count += _putchar(digit);
 	return (count);
 }
